1c05ea29c533c702e38a028cf7e008f737a33164
brushed <dirk.frederickx@gmail.com>
2013-06-22 22:58:52 +0200

    Replace global LocalizedStrings by I18N

--- src/localize.js
+++ src/localize.js
@@ -3,15 +3,15 @@ Function: localize
     Localize a string with optional parameters. (indexed or named)
 
 Require:
-    LocalizedString - (global hash) It stores all the {{name:value}} pairs
-        name - starts with a prefix {{javascript.}}.
+    I18N - (global hash) repository of {{name:value}} pairs
+        name - starts with a prefix {{javascript.}}
         value - (server generated) localized string, with optional {parameters}
-            in curly braces.
+            in curly braces
 
 Examples:
     (start code)
-    //initialize the i18n strings
-    var LocalizedStrings = {
+    //initialize the translation strings
+    var I18N = {
         "javascript.moreInfo":"More",
         "javascript.imageInfo":"Image {0} of {1}",  //indexed parms
         "javascript.imageInfo2":Image {imgCount} of {totalCount}"   //named parms
@@ -20,19 +20,19 @@ Examples:
 
     "moreInfo".localize() === "More";
     "imageInfo".localize(2,4) ===  "Image 2 of 4"
-    "imageInfo2".localize({imgCount:2, totalCount:4}) === "Image 2 of 4"
+    "imageInfo2".localize({totalCount:4, imgCount:2}) === "Image 2 of 4"
     "curlyBraces".localize() === "Show {Curly Braces}"
 
     (end)
 
 Note:
-    Make sure that this.LocalizedStrings is defined prior to binding it to the .localize function
+    Make sure that this.I18N is defined prior to binding it to the .localize function
 */
-!function(i18nStrings, i18nprefix){
+!function(I18N, prefix){
 
     String.implement('localize', function( params ){
 
-            return ( i18nStrings[i18nprefix+this] || this ).substitute(
+            return ( I18N[prefix+this] || this ).substitute(
                 ( typeOf(params) == 'object' ) ? params : Array.from(arguments)
             );
 
@@ -40,4 +40,4 @@ Note:
 
     );
 
-}( this.LocalizedStrings, 'javascript.');
+}( this.I18N, 'javascript.');
