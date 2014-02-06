b1fbe2fc083f915dff1ca4bb964c84e6c801b706
brushed <dirk.frederickx@gmail.com>
2014-02-06 21:49:22 +0100

    Replace tabs by spaces

--- src/Form.MultipleFile.js
+++ src/Form.MultipleFile.js
@@ -12,48 +12,48 @@ provides: Form.MultipleFileInput
 */
 
 Object.append(Element.NativeEvents, {
-	dragenter: 2, dragleave: 2, dragover: 2, dragend: 2, drop: 2
+    dragenter: 2, dragleave: 2, dragover: 2, dragend: 2, drop: 2
 });
 
 if (!this.Form) this.Form = {};
 
 Form.MultipleFile = new Class({
 
-	Implements: [Options, Events],
+    Implements: [Options, Events],
 
-	options: {
+    options: {
         /*
-		onAdd: function(file){},
-		onRemove: function(file){},
-		onEmpty: function(){},
-		onDragenter: function(event){},
-		onDragleave: function(event){},
-		onDragover: function(event){},
-		onDrop: function(event){}*/
-	},
+        onAdd: function(file){},
+        onRemove: function(file){},
+        onEmpty: function(){},
+        onDragenter: function(event){},
+        onDragleave: function(event){},
+        onDragover: function(event){},
+        onDrop: function(event){}*/
+    },
 
-	_files: [],
+    _files: [],
 
-	initialize: function(input, list, drop, options){
-	
-		input = this.element = document.id(input);
-		list = this.list = document.id(list);
-		drop = document.id(drop);
+    initialize: function(input, list, drop, options){
+    
+        input = this.element = document.id(input);
+        list = this.list = document.id(list);
+        drop = document.id(drop);
 
-		this.setOptions(options);
+        this.setOptions(options);
 
-		var self = this,
-		    name = input.get('name');
+        var self = this,
+            name = input.get('name');
 
-		if (name.slice(-2) != '[]') input.set('name', name + '[]');
-		input.set('multiple', true);
+        if (name.slice(-2) != '[]') input.set('name', name + '[]');
+        input.set('multiple', true);
 
         list.addEvents({
             'change:relay(input)': function(e){
-				Array.each(input.files, self.add, self);
-				self.fireEvent('change', event);
+                Array.each(input.files, self.add, self);
+                self.fireEvent('change', event);
             },
-		    'click:relay(a.delete)': function(e){ self.remove(this.retrieve('file')); }
+            'click:relay(a.delete)': function(e){ self.remove(this.retrieve('file')); }
         });
 
         function activateDrop(event, isOn){
@@ -64,71 +64,71 @@ Form.MultipleFile = new Class({
         if(drop && (typeof document.body.draggable != 'undefined')){
         
             input.addEvents({
-			dragenter: function(event){ activateDrop(event,true); }, //self.fireEvent.bind(self, 'dragenter'),
-			dragleave: function(event){ activateDrop(event,false); }, //self.fireEvent.bind(self, 'dragleave'),
-			dragend: self.fireEvent.bind(self, 'dragend'),
-			dragover: function(event){ 
-				event.preventDefault();
-				self.fireEvent(event.type, event);
-			},
-			drop: function(event){
-				event.preventDefault();
-				var dataTransfer = event.event.dataTransfer;
-				if (dataTransfer) Array.each(dataTransfer.files, self.add, self);
-				activateDrop(event, false);
-			}
+            dragenter: function(event){ activateDrop(event,true); }, //self.fireEvent.bind(self, 'dragenter'),
+            dragleave: function(event){ activateDrop(event,false); }, //self.fireEvent.bind(self, 'dragleave'),
+            dragend: self.fireEvent.bind(self, 'dragend'),
+            dragover: function(event){ 
+                event.preventDefault();
+                self.fireEvent(event.type, event);
+            },
+            drop: function(event){
+                event.preventDefault();
+                var dataTransfer = event.event.dataTransfer;
+                if (dataTransfer) Array.each(dataTransfer.files, self.add, self);
+                activateDrop(event, false);
+            }
             });
 /*        
             input.addEvents({
-			dragenter: function(e){ activateDrop(true); console.log(e.type); self.fireEvent('dragenter'); },
-			dragleave: self.fireEvent.bind(self, 'dragleave'),
-			dragend: self.fireEvent.bind(self, 'dragend'),
-			dragover: function(event){
-				event.preventDefault();
-				self.fireEvent('dragover', event);
-			},
-			drop: function(event){
-				event.preventDefault();
-				var dataTransfer = event.event.dataTransfer;
-				if (dataTransfer) Array.each(dataTransfer.files, self.add, self);
-	            console.log("input value ",input.value);
-				self.fireEvent('drop', event);
-			}
+            dragenter: function(e){ activateDrop(true); console.log(e.type); self.fireEvent('dragenter'); },
+            dragleave: self.fireEvent.bind(self, 'dragleave'),
+            dragend: self.fireEvent.bind(self, 'dragend'),
+            dragover: function(event){
+                event.preventDefault();
+                self.fireEvent('dragover', event);
+            },
+            drop: function(event){
+                event.preventDefault();
+                var dataTransfer = event.event.dataTransfer;
+                if (dataTransfer) Array.each(dataTransfer.files, self.add, self);
+                console.log("input value ",input.value);
+                self.fireEvent('drop', event);
+            }
             });
 */
         }
 
-	},
+    },
 
-	add: function(file){
-	
-		this._files.push(file);
-		
+    add: function(file){
+    
+        this._files.push(file);
+        
         var newItem = this.list.getFirst().clone(true,true);          
-	    newItem.getElement('input').destroy();
-	    newItem.getElement('label').set('html', file.name + "<b>"+file.size/1024+" Kb</b>" );
-		newItem.getElement('.delete').removeClass('hidden').store('file',file);
-		newItem.removeClass('droppable');
-    	this.list.grab(newItem);
+        newItem.getElement('input').destroy();
+        newItem.getElement('label').set('html', file.name + "<b>"+file.size/1024+" Kb</b>" );
+        newItem.getElement('.delete').removeClass('hidden').store('file',file);
+        newItem.removeClass('droppable');
+        this.list.grab(newItem);
 
-		this.fireEvent('add', file);
-		return this;
-	},
+        this.fireEvent('add', file);
+        return this;
+    },
 
-	remove: function(file){
+    remove: function(file){
 
-		var index = this._files.indexOf(file);
+        var index = this._files.indexOf(file);
 
-		if (index != -1){
-    		this._files.splice(index, 1);
-	    	this.list.getChildren()[index+1].destroy();
-		    this.fireEvent('remove', file);
-		}
-		return this;
-	},
+        if (index != -1){
+            this._files.splice(index, 1);
+            this.list.getChildren()[index+1].destroy();
+            this.fireEvent('remove', file);
+        }
+        return this;
+    },
 
-	getFiles: function(){
-		return this._files;		
-	}
+    getFiles: function(){
+        return this._files;        
+    }
 
 });
