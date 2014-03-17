3cbb4917a3572f71a0355641baca3d0444abca8a
brushed <dirk.frederickx@gmail.com>
2014-03-17 20:01:05 +0100

    Small optimisations

--- src/Accesskey.js
+++ src/Accesskey.js
@@ -1,7 +1,8 @@
 /*
 Function: Accesskey
     Highlight the available accesskeys to the user:
-    - underline the accesskey ( wrap inside span.accesskey )
+    - wrap the access key inside a span.accesskey to apply css style 
+      (eg. underlining the access key)
     - add a suffix to the title attribute of the element with the accesskey
       in square brackets : "title [ key ]"
 
@@ -12,21 +13,25 @@ Arguments:
 
 function Accesskey(element, template){
 
-    if( !element.getElement('span.accesskey') ){
+    var accesskey = 'accesskey',
+        key = element.get(accesskey),
+        title = element.get('title');
 
-        var key = element.get('accesskey'),
-            title = element.get('title');
+    //we don't do double span.accesskeys
+    if( !element.getElement('span.'+accesskey) ){
 
         if( key ){
 
             element.set({
                 html: element.get('html').replace(
                     RegExp( '('+key+')', 'i'),
-                    template || "<span class='accesskey'>$1</span>" )
+                    template || "<span class='"+accesskey+"'>$1</span>"
+                )
             });
-            if( title ){ element.set('title', title + ' [ '+key+' ]'); }
 
-           //console.log("ACCESSKEY ::",key, element.get('text'), element.get('title') );
+            if(title){ element.set('title', title + ' [ '+key+' ]'); }
+
+            //console.log("ACCESSKEY ::",key, element.get('text'), element.get('title') );
 
         }
     }
