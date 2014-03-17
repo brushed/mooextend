d85529d54b8cbc3040e97a7b1f190d2d03925b16
brushed <dirk.frederickx@gmail.com>
2014-03-17 19:24:54 +0100

    Small optimisations

--- src/Array.Extend.HSV.js
+++ src/Array.Extend.HSV.js
@@ -1,74 +1,76 @@
 /*
 Function: hsv2rgb
-	Convert HSV values into RGB values
+    Convert HSV values into RGB values
 */
 Array.implement({
-	hsv2rgb: function(){
-		// easyrgb.com/math.php?MATH=M21#text21
-		var r,A,B,C,F,
-			self = this,
-			hue = self[0]/360, //hue - degrees from 0 to 360
-			sat = self[1]/100, //saturation - %
-			val = self[2]/100; //value - %
 
-		if( !sat/*sat==0*/ ){
+    hsv2rgb: function(){
+        // easyrgb.com/math.php?MATH=M21#text21
+        var r,A,B,C,F,
+            self = this,
+            hue = self[0]/360, //hue - degrees from 0 to 360
+            sat = self[1]/100, //saturation - %
+            val = self[2]/100; //value - %
 
-			r=[val,val,val];
+        if( !sat/*sat==0*/ ){
 
-		} else {
+            r=[val,val,val];
 
-	        hue = (hue>1) ? 0 : 6 * hue;
-	        F = Math.floor(hue);
-	        A = val * (1 - sat);
-	        B = val * (1 - sat * (hue-F) );
-	        C = val * (1 - sat * ( 1 - (hue-F)) );
-	        //val = Math.round(val);
+        } else {
 
-			r = (!F/*F==0*/) ? [val,C,A] : (F==1) ? [B,val,A] : (F==2) ? [A,val,C]:
-				(F==3) ? [A,B,val] : (F==4) ? [C,A,val] : [val,A,B];
+            hue = (hue>1) ? 0 : 6 * hue;
+            F = hue|0; //Math.floor(hue);
+            A = val * (1 - sat);
+            B = val * (1 - sat * (hue-F) );     
+            C = val * (1 - sat * ( 1 - (hue-F)) );
+            //val = Math.round(val);
 
-	    }
+            r = (F==0) ? [val, C, A] : 
+                (F==1) ? [B, val, A] : 
+                (F==2) ? [A, val ,C] :
+                (F==3) ? [A, B, val] : 
+                (F==4) ? [C, A, val] : [val, A, B];
 
-		return r.map(function(x){return Math.round(x*255); });
+        }
 
-	},
-	rgb2hsv: function(){
+        console.log('hsv2rgb')
+        return r.map(function(x){return Math.round(x*255); });
+        //return [ r[0]*255, r[1]*255, r[2]*255 ];
 
-		var hue, //hue - degrees from 0 to 360
-			sat, //saturation- %
-			val, //value - %
-			self = this,
-			r = self[0]/255,
-			g = self[1]/255,
-			b = self[2]/255,
-			d2,dR,dG,dB,
-			maxVal = Math.max(r, g, b),
-			delta = maxVal - Math.min(r, g, b);
+    },
+    
+    rgb2hsv: function(){
 
-		//val = Math.round( maxVal*100 );
-		val = maxVal*100;
+        var hue=0, //hue - degrees from 0 to 360
+            sat=0, //saturation- %
+            self = this,
+            r = self[0]/255,
+            g = self[1]/255,
+            b = self[2]/255,
+            d2,dR,dG,dB,
+            maxVal = Math.max(r, g, b),
+            //val = Math.round( maxVal*100 )
+            val = maxVal*100, //value - %
+            delta = maxVal - Math.min(r, g, b);
 
-		if( !delta /*delta==0*/ ){		//This is a gray, no chroma..
+                        //if delta==0 : this is a gray, no chroma..
+        if( delta ){    //else chromatic data
 
-			return [0, 0, val]; //[hue,sat,val]
+            sat = delta / maxVal;
+            d2 = delta / 2;
+            dR = (((maxVal - r) / 6) + d2) / delta;
+            dG = (((maxVal - g) / 6) + d2) / delta;
+            dB = (((maxVal - b) / 6) + d2) / delta;
 
-		} else {				//Chromatic data
+            hue = (r == maxVal) ? dB - dG
+                : (g == maxVal) ? (1/3) + dR - dB
+                : /*b == maxVal*/ (2/3) + dG - dR;
 
-			sat = delta / maxVal;
-			d2 = delta / 2;
-			dR = (((maxVal - r) / 6) + d2) / delta;
-			dG = (((maxVal - g) / 6) + d2) / delta;
-			dB = (((maxVal - b) / 6) + d2) / delta;
+            if (hue < 0) { hue += 1; }
+            if (hue > 1) { hue -= 1; }
 
-			hue = (r == maxVal)  ? dB - dG
-				: (g == maxVal)  ? (1/3) + dR - dB
-				: /*b == maxVal*/ (2/3) + dG - dR;
-
-			if (hue < 0) { hue += 1; }
-			if (hue > 1) { hue -= 1; }
-
-			//return[ Math.round(hue*360), sat*100, val];
-			return[ hue*360, sat*100, val];
-		}
-	}
+        }
+        
+        return[ Math.round(hue*360), sat*100, val];
+    }
 });
