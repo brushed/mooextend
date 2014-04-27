7d8a180dd4a55b0f3d22b4629b40c1f0ee4b5230
brushed <dirk.frederickx@gmail.com>
2014-04-27 16:54:12 +0200

    Small code updates

--- src/Accesskey.js
+++ src/Accesskey.js
@@ -8,31 +8,38 @@ Function: Accesskey
 
 Arguments:
     element - DOM element
-    template - (string) html template replacement string, default <span class='accesskey'>$1</span>
+    
+Example:
+(start code)
+    new Accesskey( $('#menu) );
+
+    //before
+    <a id="menu" accesskey="m" title="main menu">Menu</a>
+
+    //after
+    <a id="menu" accesskey="m" title="main menu [m]"><span class="accesskey">M</span>enu</a>
+
+(end)
 */
 
-function Accesskey(element, template){
+function Accesskey(element){
 
     var accesskey = 'accesskey',
         key = element.get(accesskey),
         title = element.get('title');
 
-    //we don't do double span.accesskeys
-    if( !element.getElement('span.'+accesskey) ){
+    if( key && !element.getElement('span.'+accesskey) ){
 
-        if( key ){
+        element.set({
+            html: element.get('html').replace(
+                RegExp( '('+key+')', 'i'),
+                "<span class='"+accesskey+"'>$1</span>"
+            )
+        });
 
-            element.set({
-                html: element.get('html').replace(
-                    RegExp( '('+key+')', 'i'),
-                    template || "<span class='"+accesskey+"'>$1</span>"
-                )
-            });
+        if(title){ element.set('title', title + ' ['+key+']'); }
 
-            if(title){ element.set('title', title + ' [ '+key+' ]'); }
+        //console.log("ACCESSKEY ::",key, element.get('text'), element.get('title') );
 
-            //console.log("ACCESSKEY ::",key, element.get('text'), element.get('title') );
-
-        }
     }
 }
